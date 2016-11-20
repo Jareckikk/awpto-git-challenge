64c35dd272e4ee877b38a38e0bbf94a6132bb562
Jarosław Kozak <jarekk86@gmail.com>
2016-11-20 11:38:23 +0100

    union update

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -12,6 +12,18 @@ public class UF {
 	}
 	
 	public void union(int p, int q) {
+		int i = root(p);
+		int j = root(q);
+		if (i == j) return;
+		 if (sz[i] < sz[j]) {
+		 id[i] = j;
+		 sz[j] += sz[i];
+		 }
+		else {
+		 id[j] = i;
+		 sz[i] += sz[j];
+		}
+
 	}
 	
 	public boolean connected(int p, int q) {
